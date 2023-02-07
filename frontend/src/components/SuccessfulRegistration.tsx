dfaa539e6c48fc520a91bf98b5bee2162c05a624
AXG25 <axiever.guejia@gmail.com>
2023-02-07 16:39:21 -0500

    Update SuccessfulRegistration.tsx

--- frontend/src/components/SuccessfulRegistration.tsx
+++ frontend/src/components/SuccessfulRegistration.tsx
@@ -6,7 +6,7 @@ const SuccessfulRegistration: React.FC = () => {
     <div className="flex items-center justify-center h-screen">
       <div className="bg-white p-6 text-center">
       <FiCheckCircle className=" text-5xl text-green-600 m-auto" />
-        <h1 className="text-2xl font-medium my-4">Registro exitoso</h1>
+        <h2 className="text-2xl font-medium my-4">Registro exitoso</h2>
         <p className="text-gray-600">Gracias por registrarte en nuestra plataforma.</p>
         <button className="bg-green-600 text-white py-2 px-4 rounded-full mt-4 hover:bg-green-700">Completar perfil</button>
         <p className="text-center mt-4 text-gray-600">
