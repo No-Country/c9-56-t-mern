88e74751a3ed757957f5686c8087bf32e240275f
AXG25 <axiever.guejia@gmail.com>
2023-02-07 11:48:42 -0500

    Created the login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import LoginForm from './components/LoginForm';
 
 function App() {
 
   return (
     <div className="App">
      <h1 className="text-lime-600">Hello World</h1>
+     <LoginForm
+        title="Iniciar Sesión"
+        welcomeText="Bienvenido al sistema, por favor inicia sesión"
+      />
     </div>
   )
 }
