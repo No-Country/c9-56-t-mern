ec7007ef6e5be190918d34226f8a2e8041db2a31
VictorMRJDev <victortecno98@gmail.com>
2023-02-13 01:46:38 -0600

    Correción de importaciones

--- frontend/src/components/registerForm/RegisterForm.jsx
+++ frontend/src/components/registerForm/RegisterForm.jsx
@@ -1,64 +1,67 @@
 import React, { useState } from "react";
-import RegisterFormStep1 from "./components/registerForm/RegisterFormStep1";
-import RegisterFormStep2 from "./components/registerForm/RegisterFormStep2";
-import StepNavigation from "./components/stepper/StepNavigation";
+import React, { useState } from "react";
+import RegisterFormStep2 from "./RegisterFormStep2";
+import RegisterFormStep1 from "./RegisterFormStep1";
+import StepNavigation from "../stepper/StepNavigation";
 
 
-const RegisterForm =() => {
+
+const RegisterForm = () => {
   // Registro Form
   const labelArray = ['Step 1', 'Step 2']
   const [currentStep, setCurrentStep] = useState(1);
-  const updateStep = (step)=> {
+  const updateStep = (step) => {
     setCurrentStep(step);
   }
- const getInfoPerPage = () =>{
-if(currentStep === 1){
-  return (
-    <div>
-<RegisterFormStep1 
-labelArray={labelArray}  
-currentStep={currentStep} 
-setCurrentStep={setCurrentStep}
-updateStep={updateStep}/>
-    </div>
-  )}
-  else {
-    return (
-      <div >
-  <RegisterFormStep2 />
-    </div>
-    )
-  }
+  const getInfoPerPage = () => {
+    if (currentStep === 1) {
+      return (
+        <div>
+          <RegisterFormStep1
+            labelArray={labelArray}
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+            updateStep={updateStep} />
+        </div>
+      )
+    }
+    else {
+      return (
+        <div >
+          <RegisterFormStep2 />
+        </div>
+      )
+    }
 
- }
+  }
 
 
   return (
     <div className="App">
-      <StepNavigation 
-      labelArray={labelArray} 
-      currentStep={currentStep} 
-      updateStep={updateStep}>
+      <StepNavigation
+        labelArray={labelArray}
+        currentStep={currentStep}
+        updateStep={updateStep}>
 
       </StepNavigation>
       <h2><strong>Registro</strong></h2>
-    <br />
-       <br />
-    <p>Texto de Bienvenida</p>
-    <br />
-    <br />
+      <br />
+      <br />
+      <p>Texto de Bienvenida</p>
+      <br />
+      <br />
 
       {getInfoPerPage()}
 
       <p>Ya tienes una cuenta? <a href="http://">Inciar Sesion</a> </p>
       <br />
-            <br />
+      <br />
       <button className="primaryButton" disabled={currentStep === 1} onClick={() => updateStep(currentStep - 1)}>Back</button>
-      <button className="primaryButton" disabled={currentStep === labelArray.length} onClick={() => updateStep(currentStep+1)}>Next</button>
+      <button className="primaryButton" disabled={currentStep === labelArray.length} onClick={() => updateStep(currentStep + 1)}>Next</button>
 
 
-    </div> 
-  ) 
+    </div>
+  )
 }
 
 
