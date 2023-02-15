e63799a3726af07fa836541da9d8879302758741
Luce <Lucerogonzalez1997@gmail.com>
2023-02-15 03:05:27 -0300

    add (index.css) styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import FormPropietario from "./components/profOwner/FormPropietario"
 import PerfilPropietario from './components/profOwner/PerfilPropietario';
 import ProfilePet from './components/profPet/ProfilePet';
+import HomePage from './components/home/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-lime-600">Hello World</h1>
+      <h1 className="text-lime-600">Pawful</h1>
+      <HomePage />
       {/* <FormPropietario/> */}
-      <PerfilPropietario/>
-      <ProfilePet/>
+      // <PerfilPropietario/>
+     // <ProfilePet/>
+      
     </div>
   )
 }
