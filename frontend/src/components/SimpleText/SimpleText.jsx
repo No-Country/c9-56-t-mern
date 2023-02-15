7538ed28906c7afd6c875da189a9deee9e5aef24
AXG25 <axiever.guejia@gmail.com>
2023-02-14 19:35:08 -0500

    feat(components/SimpleText)Structure created

--- frontend/src/components/SimpleText/SimpleText.jsx
+++ frontend/src/components/SimpleText/SimpleText.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react"
 
-const SimpleText = () => {
+const SimpleText = ({ title, paragraph }) => {
   return (
-    <div>
-      
+    <div className="mx-auto w-full text-center px-4 py-6 bg-gray-50  text-neutral-900">
+      <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+      <p className="text-xl font-normal  ">{paragraph}</p>
     </div>
   )
 }
 
 export default SimpleText
+
+{
+  //ejemplo de importación
+  /* <SimpleText 
+        title={'Registro'}
+        paragraph={'¡Falta poco! Ya casi tienes tu cuenta en Pawful. '}
+      /> */
+}
