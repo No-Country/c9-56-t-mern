1289b49d8423b22366c67c06e7850e47d1830b0d
aouriarte <uriarte2001alexis@gmail.com>
2023-02-16 18:58:43 -0500

    fix(Home/CategoriesHome): get card different side

--- frontend/src/components/home/CategoriesHome.jsx
+++ frontend/src/components/home/CategoriesHome.jsx
@@ -1,68 +1,45 @@
-import React from 'react';
-import CardOne from '../CardOne/CardOne'
-import CardTwo from '../CardTwo/CardTwo';
+import React from "react"
+import CardOne from "../CardOne/CardOne"
+import CardTwo from "../CardTwo/CardTwo"
+
 const CategoriesHome = () => {
-    let categoryDetails = [{
-        category: 'Peluquería',
-        categoryDescription: 'Servicios de baño, corte y estéticos para perros y gatos.'
-
+  let categoryDetails = [
+    {
+      category: "Peluquería",
+      categoryDescription:
+        "Servicios de baño, corte y estéticos para perros y gatos.",
     },
     {
-        category: 'Guardería',
-        categoryDescription: 'Espacios de cuidado para tu animal de compañía.'
-
+      category: "Guardería",
+      categoryDescription: "Espacios de cuidado para tu animal de compañía.",
     },
     {
-        category: 'Pastelería',
-        categoryDescription: 'Catering para fiestas, pasteles, tortas y bocados.'
-
+      category: "Pastelería",
+      categoryDescription: "Catering para fiestas, pasteles, tortas y bocados.",
     },
     {
-        category: 'Fiestas',
-        categoryDescription: 'Organización de eventos, decoración y animación.'
-
+      category: "Fiestas",
+      categoryDescription: "Organización de eventos, decoración y animación.",
     },
     {
-        category: 'Paseo',
-        categoryDescription: 'Salidas por hora en grupo o solitario.'
+      category: "Paseo",
+      categoryDescription: "Salidas por hora en grupo o solitario.",
+    },
+  ]
 
-    }
-    ]
-   
-    return (
-        categoryDetails.map(categories => (
-        <CardOne
-                service={categories.category}
-                paragraph={categories.categoryDescription}
-        
-                />) ))
+  return categoryDetails.map((categories) =>
+    categories.category === "Guardería" || categories.category === "Fiestas" ? (
+      <CardTwo
+        service={categories.category}
+        paragraph={categories.categoryDescription}
+      />
+    ) : (
+      <CardOne
+        service={categories.category}
+        paragraph={categories.categoryDescription}
+      />
+    ),
+  )
+}
 
-};     // ACA LE VOY A AGREGAR LOGICA PARA QUE CAMBIE DE LADO
-
-
-// GetCardDifferentSide = () => {
-//     categoryDetails.map(categories => (
-                    
-//         <CardTwo
-//         service={categories.category}
-//         paragraph={categories.categoryDescription}
-
-//         /> )) 
-//     if ({service} === 'Guardería' || 'Fiestas')
-//     return (  <CardTwo
-//         service={categories.category}
-//         paragraph={categories.categoryDescription}
-
-//         /> )
-//         else
-//         return (
-//             <CardOne 
-//         service={categories.category}
-//         paragraph={categories.categoryDescription}
-
-//        lo  /> ) 
-
-        
-//    }
-export default CategoriesHome;
-
+export default CategoriesHome
