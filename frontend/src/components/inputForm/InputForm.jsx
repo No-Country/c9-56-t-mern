e7312446236929b843ef9b773599dbcf3c21fd24
Jamir Edgar Bances Espinoza <jbances7@gmail.com>
2023-02-16 01:21:56 -0500

    update(components): InputForm.jsx

--- frontend/src/components/inputForm/InputForm.jsx
+++ frontend/src/components/inputForm/InputForm.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useState } from "react"
 
-const InputForm = ({label, placeholder }) => {
+const InputForm = ({ label, placeholder }) => {
   const [value, setValue] = useState("")
 
   const handleChange = (e) => {
@@ -11,9 +11,11 @@ const InputForm = ({label, placeholder }) => {
   return (
     <div className="flex flex-col m-auto max-w-xs bg-gray-50 py-4">
       <div className="container flex flex-col justify-center  max-w-sm">
-        <label htmlFor="" className="py-2">{label}</label>
+        <label htmlFor="" className="py-2">
+          {label}
+        </label>
         <input
-          className="border border-neutral-200 rounded-lg pl-2 w-full"
+          className="border border-neutral-200 rounded-lg h-10 pl-2 w-full"
           type="text"
           placeholder={placeholder}
           value={value}
