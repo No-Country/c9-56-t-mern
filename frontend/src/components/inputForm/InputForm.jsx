5f04bb1e5cf4df6d3aa8fb60b5b1837ce92e1206
Jamir Edgar Bances Espinoza <jbances7@gmail.com>
2023-02-15 22:57:19 -0500

    add(components): InputForm

---
+++ frontend/src/components/inputForm/InputForm.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { useState } from "react"
+
+const InputForm = ({label, placeholder }) => {
+  const [value, setValue] = useState("")
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  return (
+    <div className="flex flex-col m-auto max-w-xs bg-gray-50 py-4">
+      <div className="container flex flex-col justify-center  max-w-sm">
+        <label htmlFor="" className="py-2">{label}</label>
+        <input
+          className="border border-neutral-200 rounded-lg pl-2 w-full"
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default InputForm
