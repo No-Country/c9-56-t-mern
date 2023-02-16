9025180502085c086720e796fbaefac34914bda5
Luce <Lucerogonzalez1997@gmail.com>
2023-02-16 11:29:24 -0300

    recycle (home/) btn & card components

--- frontend/src/components/MainBtn/MainBtn.jsx
+++ frontend/src/components/MainBtn/MainBtn.jsx
@@ -5,7 +5,7 @@ const MainBtn = ({ text }) => {
     <div className="flex bg-gray-50 py-4">
       <button className="max-w-xs bg-violet-800 w-full m-auto px-6 py-3 rounded-lg text-gray-50 text-sm font-medium uppercase">{text}      </button>
     </div>
-  )
+  ) 
 }
 
 export default MainBtn
