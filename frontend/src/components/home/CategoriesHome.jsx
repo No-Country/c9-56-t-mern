d9e9f52467321da75fa68877372f8afed3b26255
Luce <Lucerogonzalez1997@gmail.com>
2023-02-16 15:44:33 -0300

    change (homePage/) cards

--- frontend/src/components/home/CategoriesHome.jsx
+++ frontend/src/components/home/CategoriesHome.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import CardOne from '../CardOne/CardOne'
+import CardTwo from '../CardTwo/CardTwo';
 const CategoriesHome = () => {
     let categoryDetails = [{
         category: 'Peluquería',
@@ -27,35 +28,41 @@ const CategoriesHome = () => {
 
     }
     ]
+   
     return (
-        <div className="max-w-sm mt-5 mx-auto p-6 rounded-lg flex flex-col items-center">
-        <div className="flex">
-            {/* div imagen cat */}
-            <div className="flex justify-center items-center w-99 h-99 bg-gray-500">
+        categoryDetails.map(categories => (
+        <CardOne
+                service={categories.category}
+                paragraph={categories.categoryDescription}
+        
+                />) ))
 
-            </div>
-            {/* div categoriaName y text */}
-            {
-                categoryDetails.map(categories => (
+};     // ACA LE VOY A AGREGAR LOGICA PARA QUE CAMBIE DE LADO
 
-                    <div className="flex justify-center">
-  <div className="max-w-sm mx-4 my-4 h-32 px-2 py-4 bg-gray-50 drop-shadow-md rounded-2xl flex justify-around items-center text-center text-neutral-900">
-    <div className="bg-gray-500 w-24 h-24 rounded-full">
-      {/* <img src={image} alt='service' className='object-cover w-full h-full rounded-full' /> */}
-    </div>
-    <div className="w-56 h-20">
-      <h2 className="text-2xl font-semibold">{categories.category}</h2>
-      <p className="text-base font-normal">{categories.categoryDescription}
-        Servicios de baño, corte y estéticos para perros y gatos.
-      </p>
-    </div>
-  </div>
-</div>
-                    ))
-                }
-            </div>
-        </div>
-    );
-};
 
+// GetCardDifferentSide = () => {
+//     categoryDetails.map(categories => (
+                    
+//         <CardTwo
+//         service={categories.category}
+//         paragraph={categories.categoryDescription}
+
+//         /> )) 
+//     if ({service} === 'Guardería' || 'Fiestas')
+//     return (  <CardTwo
+//         service={categories.category}
+//         paragraph={categories.categoryDescription}
+
+//         /> )
+//         else
+//         return (
+//             <CardOne 
+//         service={categories.category}
+//         paragraph={categories.categoryDescription}
+
+//         /> )
+
+        
+//    }
 export default CategoriesHome;
+
