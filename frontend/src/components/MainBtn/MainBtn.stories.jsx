70f62985e597ccd8051c6b90cf41dc31a7a6fafd
Pietro Palombini <pietropalo@icloud.com>
2023-02-17 04:41:41 -0300

    doc(MainBtn): update MainBtn story

--- frontend/src/components/MainBtn/MainBtn.stories.jsx
+++ frontend/src/components/MainBtn/MainBtn.stories.jsx
@@ -4,11 +4,20 @@ export default {
   title: "Components/MainBtn",
   component: MainBtn,
   args: {},
+  argTypes: {
+    type: {
+      control: {
+        type: "select",
+        options: ["button", "submit", "reset"],
+      },
+    },
+  },
 }
 
 const Template = (args) => <MainBtn {...args} />
 
-export const SampleText = Template.bind({})
-SampleText.args = {
+export const Sample = Template.bind({})
+Sample.args = {
   text: "Sample Button",
+  type: "button",
 }
