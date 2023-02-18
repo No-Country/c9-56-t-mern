edfc4f95a33fa7c4f9f62d6f615d91c86972644c
Pietro Palombini <pietropalo@icloud.com>
2023-02-18 05:14:34 -0300

    fix(Panel): remove external margin

--- frontend/src/components/Panel/Panel.jsx
+++ frontend/src/components/Panel/Panel.jsx
@@ -6,21 +6,21 @@ const Panel = ({ title, placeholder }) => {
   const [panel, setPanel] = useState([])
 
   return (
-    <div className="flex items-center flex-col  m-6">
-      <div className="flex items-center justify-between w-full max-w-sm">
-        <h2 className="mx-4 font-bold text-xl text-neutral-900">
+    <div className="flex items-center flex-col">
+      <div className="flex items-center justify-between w-full">
+        <h2 className="font-bold text-xl text-neutral-900">
           Mis mascotas {/*{ title }*/}
         </h2>
         <AddBtn />
       </div>
       {panel.length === 0 ? (
-        <div className="py-16 px-4 w-full max-w-sm text-center">
+        <div className="py-16 px-4 w-full text-center">
           <h2 className="font-normal text-base text-neutral-900">
             Añade elementos al panel {/*{placeholder}*/}
           </h2>
         </div>
       ) : (
-        <div className="py-4 px-4 w-full max-w-sm text-center">
+        <div className="py-4 px-4 w-full text-center">
           {/* {panel.map((item, index) => (
             <li key={index}>{item}</li>
           ))} */}
