f2809debf7fedceb8e7d8cc4cd7d33ae3b8b2e9f
aouriarte <uriarte2001alexis@gmail.com>
2023-02-19 12:35:02 -0500

    refactor(api/categories): add controller and endpoint DELETE /categories/:id

--- backend/src/Contexts/Pawful/Category/application/CategoryDelete.ts
+++ backend/src/Contexts/Pawful/Category/application/CategoryDelete.ts
@@ -1,16 +1,9 @@
-import { CategoryNotFound } from "../domain/errors/CategoryNotFound"
 import { CategoryRepository } from "../domain/repositories/CategoryRepository"
 
 class CategoryDeleteOne {
   constructor(private readonly categoryRepository: CategoryRepository) {}
 
   async run(id: string): Promise<void> {
-    const categoryFound = await this.categoryRepository.findById(id)
-
-    if (!categoryFound) {
-      throw new CategoryNotFound()
-    }
-
     await this.categoryRepository.deleteOne(id)
   }
 }
