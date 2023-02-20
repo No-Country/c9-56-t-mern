8d9a76e2a095ef06e69d05e901c70bd5465b62f0
AXG25 <axiever.guejia@gmail.com>
2023-02-20 12:58:28 -0500

    fix(RadioButton)fix styles

--- frontend/src/components/RadioButton/RadioButton.jsx
+++ frontend/src/components/RadioButton/RadioButton.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const RadioButton = ({title, desc}) => {
 
   return (
-    <div className='bg-neutral-50 w-36 py-3 flex flex-col text-center border border-neutral-200 focus:border focus:border-violet-700 text-neutral-400 focus:text-neutral-900 rounded-lg mx-1 my-1'>
+    <button className='bg-neutral-50 w-36 py-3 flex flex-col items-center border-2 border-neutral-200 focus:border-2 focus:border-violet-700 text-neutral-400 focus:text-neutral-900 rounded-lg mx-1 my-1'>
       <h2 className='font-semibold text-base'>{title}</h2>
       <p className='font-normal text-sm'>{desc}</p>
-    </div>
+    </button>
   )
 }
 
