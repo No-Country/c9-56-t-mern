b9b906901bdbc3f0401ffd93d45f9a203264a9d8
Luce <Lucerogonzalez1997@gmail.com>
2023-02-20 14:11:02 -0300

    refactor(imports):rename component files to index

---
+++ frontend/src/components/simpleText/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const SimpleText = ({ title, paragraph }) => {
+  return (
+    <div className="mx-auto w-full text-center px-4 py-6 bg-gray-50  text-neutral-900">
+      <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+      <p className="text-xl font-normal  ">{paragraph}</p>
+    </div>
+  )
+}
+
+export default SimpleText
+
+ 
