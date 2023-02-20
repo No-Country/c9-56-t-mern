2ddb44b0ebe395497e7d2669576e96e463d61033
AXG25 <axiever.guejia@gmail.com>
2023-02-20 09:19:04 -0500

    add(components)RadioButtonContainer

---
+++ frontend/src/components/RadioButtonContainer/RadioButtonContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RadioButtonContainer = ({children}) => {
+  return (
+    <div className='flex flex-row flex-wrap max-w-sm'>
+      {children}
+    </div>
+  )
+}
+
+export default RadioButtonContainer
