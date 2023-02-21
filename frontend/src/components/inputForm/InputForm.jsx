09084c2986f96ecf360185393f34642bd21bf08a
Jamir Edgar Bances Espinoza <jbances7@gmail.com>
2023-02-20 19:38:23 -0500

    add (components/) InputForm.jsx & InputSelect.jsx

--- frontend/src/components/inputForm/InputForm.jsx
+++ frontend/src/components/inputForm/InputForm.jsx
@@ -9,14 +9,15 @@ const InputForm = ({ label, placeholder }) => {
   }
 
   return (
-    <div className="flex flex-col m-auto max-w-xs bg-gray-50 py-4">
-      <div className="container flex flex-col justify-center  max-w-sm">
-        <label htmlFor="" className="py-2">
+    <div className="flex flex-col m-auto max-w-xs py-4">
+      <div className="container flex flex-col justify-center max-w-sm">
+        <label htmlFor="input" className="py-2">
           {label}
         </label>
         <input
-          className="border border-neutral-200 rounded-lg pl-2 w-full"
+          className="border border-neutral-200 focus:outline-none focus:ring focus:ring-[#2684FF] h-10 rounded-lg px-2 py-3 w-full"
           type="text"
+          id="input"
           placeholder={placeholder}
           value={value}
           onChange={handleChange}
