087e4e7430fc68ef29a124c29f9a4bff269714c2
Pietro Palombini <pietropalo@icloud.com>
2023-02-21 12:42:26 -0300

    fix(FormPropietario): fix story

--- frontend/src/components/profOwner/FormPropietario.stories.jsx
+++ frontend/src/components/profOwner/FormPropietario.stories.jsx
@@ -1,9 +1,24 @@
+import { Provider } from "react-redux"
+import { MemoryRouter } from "react-router-dom"
+import { onLogin } from "../../store/auth/authSlice"
+import { store } from "../../store/store"
 import FormPropietario from "./FormPropietario"
+
+store.dispatch(onLogin({ role: ["OWNER"] }))
 
 export default {
   title: "Components/FormPropietario",
   component: FormPropietario,
   args: {},
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      </Provider>
+    ),
+  ],
 }
 
 const Template = (args) => <FormPropietario {...args} />
