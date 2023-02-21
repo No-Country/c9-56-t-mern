f3ee412b36e06a4c01a710c63938d44b0d613cb0
Jamir Edgar Bances Espinoza <jbances7@gmail.com>
2023-02-21 10:50:18 -0500

    add(component/) InputSelect.stories.jsx

---
+++ frontend/src/components/inputSelect/InputSelect.stories.jsx
@@ -0,0 +1,15 @@
+import InputSelect from "./InputSelect"
+
+export default {
+  title: "components/inputSelect",
+  component: InputSelect,
+  args: {
+    children: "InputSelect",
+  },
+};
+
+const Template = (args) => <InputSelect {...args}/>;
+
+export const Primary = Template.bind({});
+
+
