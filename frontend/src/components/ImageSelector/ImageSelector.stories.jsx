5da5c82d07939a917b939d586546ea301a732cf4
Pietro Palombini <pietropalo@icloud.com>
2023-02-20 21:59:07 -0300

    fix(InputImage): remove image prop

--- frontend/src/components/ImageSelector/ImageSelector.stories.jsx
+++ frontend/src/components/ImageSelector/ImageSelector.stories.jsx
@@ -1,14 +1,21 @@
 import React from "react"
 
-import ImageSelector from "./index"
+import ImageSelector from "."
 
 export default {
   title: "Components/ImageSelector",
   component: ImageSelector,
-  args: {},
+  args: {
+    onChange: () => {},
+  },
 }
 
 const Template = (args) => <ImageSelector {...args} />
 
-export const Sample = Template.bind({})
-Sample.args = {}
+export const SampleImage = Template.bind({})
+SampleImage.args = {
+  urlImage: "https://www.w3schools.com/howto/img_avatar.png",
+}
+
+export const NoImage = Template.bind({})
+NoImage.args = {}
--- frontend/src/components/InputImage/InputImage.jsx
+++ frontend/src/components/InputImage/InputImage.jsx
@@ -1,22 +1,10 @@
-import React, { useState } from "react"
+import React from "react"
 import ImageSelector from "../ImageSelector"
 
-const InputImage = (props) => {
-  const [image, setImage] = useState(null)
-
-  const handleImageChange = (e) => {
-    const selectedImage = e.target.files[0]
-    setImage(URL.createObjectURL(selectedImage))
-    props.onChange(e.target.files[0])
-    console.log(image)
-  }
+const InputImage = ({ urlImage, onChange }) => {
   return (
     <div className="flex flex-col items-center bg-gray-50 py-4">
-      <ImageSelector
-        image={image}
-        urlImage={props.urlImage}
-        handleImageChange={handleImageChange}
-      />
+      <ImageSelector urlImage={urlImage} handleImageChange={onChange} />
     </div>
   )
 }
