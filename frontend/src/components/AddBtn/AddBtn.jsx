2668a3e78289c9767322ee5911604532d617bdbd
Pietro Palombini <pietropalo@icloud.com>
2023-02-22 13:49:22 -0300

    feat(AddBtn): update to new design

--- frontend/src/components/AddBtn/AddBtn.jsx
+++ frontend/src/components/AddBtn/AddBtn.jsx
@@ -3,7 +3,7 @@ import { HiPlusCircle } from "react-icons/hi"
 
 const AddBtn = () => {
   return (
-    <button className="flex flex-row items-center bg-gray-50">
+    <button className="flex flex-row items-center">
       <HiPlusCircle className="text-violet-700" size={20} />
       <p className="text-violet-700 uppercase text-sm font-medium p-2">
         Agregar
