2913a835c4a11b1b12fb2c7c19f86adbec09e31e
AXG25 <axiever.guejia@gmail.com>
2023-02-21 20:46:23 -0500

    modify(components/NavBarBack):Styles

--- frontend/src/components/Navbar/NavbarBack.jsx
+++ frontend/src/components/Navbar/NavbarBack.jsx
@@ -1,16 +1,19 @@
 import React from "react"
-import { IoPawSharp } from "react-icons/io5"
 import { IoChevronBackOutline } from "react-icons/io5"
 
 const NavbarBack = () => {
   return (
     <header className="flex flex-row-reverse max-w-sm justify-between items-center m-auto bg-slate-50">
-      <span className="w-full flex justify-center text-violet-700 py-4 mr-3 text-2xl">
-        <IoPawSharp />
+      <span className="w-full flex justify-center text-violet-700 py-4 mr-11 text-2xl">
+        <img
+          className="h-6 w-6"
+          src="https://res.cloudinary.com/axiever/image/upload/v1677028424/Logo-principal_s7b5oq.svg"
+          alt=""
+        />
       </span>
 
       <button className="text-lg text-violet-700 px-1.5 py-1.5 border-2 border-neutral-200 rounded-xl  focus:border-violet-700 ml-4">
-        <IoChevronBackOutline/>
+        <IoChevronBackOutline />
       </button>
     </header>
   )
