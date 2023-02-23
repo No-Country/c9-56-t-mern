0a2367fde53690febf3a2bc71fb73e94a73e034e
AXG25 <axiever.guejia@gmail.com>
2023-02-23 13:12:25 -0500

    sort(pages):imports

--- frontend/src/pages/shared/Login/LoginForm.jsx
+++ frontend/src/pages/shared/Login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useAuthStore } from "../../../hooks/useAuthStore"
-import "../../styles/styles.css"
+import "../../../styles/styles.css"
 import PurpleButton from "../../../components/PurpleButton"
 
 const LoginForm = () => {
