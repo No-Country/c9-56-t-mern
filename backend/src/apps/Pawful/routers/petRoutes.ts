50f9bccb6a09c48ee1b66429d65a150c0ca9c341
Pietro Palombini <pietropalo@icloud.com>
2023-02-23 17:28:34 -0300

    feat(Pet): create GET /pets route

--- backend/src/apps/Pawful/routers/petRoutes.ts
+++ backend/src/apps/Pawful/routers/petRoutes.ts
@@ -2,8 +2,18 @@ import { Router, type Request, type Response } from "express"
 
 import { asyncHandler } from "../../shared/framework/asyncHandler"
 import { CreatePetController } from "../controllers/pet/CreatePetController"
+import { FindPetsController } from "../controllers/pet/FindPetsController"
 
 const petRouter = Router()
+
+petRouter.get(
+  "/pets",
+  asyncHandler(async (req: Request, res: Response) => {
+    const findPetsController = new FindPetsController()
+
+    await findPetsController.run(req, res)
+  }),
+)
 
 petRouter.post(
   "/pets",
