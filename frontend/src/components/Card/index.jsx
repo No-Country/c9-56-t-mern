f20877a667526e03145cded4553ff1e498595757
AXG25 <axiever.guejia@gmail.com>
2023-02-24 22:45:36 -0500

    fix(components/CoverPhoto)NoImage

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -15,7 +15,7 @@ const Card = ({ imageSide = "left", image, service, paragraph }) => {
         className={`max-w-sm mx-4 w-full  my-4 h-32 px-2 py-4 bg-gray-50 drop-shadow-md rounded-2xl flex ${flexDirection} justify-around items-center text-center text-neutral-900`}
       >
         <div className="bg-gray-500 w-24 h-24 max-w-sm rounded-full">
-          {/* <img src={image} alt='service' className='object-cover w-full h-full rounded-full' /> */}
+          <img src={image} alt='service' className='object-cover w-full h-full rounded-full' />
         </div>
         <div className="w-56 h-20">
           <h2 className="text-2xl font-semibold">{service}</h2>
--- frontend/src/components/CoverPhoto/CoverPhoto.jsx
+++ frontend/src/components/CoverPhoto/CoverPhoto.jsx
@@ -5,32 +5,32 @@ import PencilIcon from "../PencilIcon"
 const CoverPhoto = ({ urlImage, onChange }) => {
   return (
     <div>
-      <div className="max-w-sm w-full m-auto h-44 rounded-full bg-slate-600 flex items-center justify-center relative">
-      {urlImage ? (
-        <img
-          src={urlImage}
-          alt="profile"
-          className="object-cover w-full h-full"
+      <div className="max-w-sm w-full m-auto h-44 bg-violet-600 flex items-center justify-center relative">
+        {urlImage ? (
+          <img
+            src={urlImage}
+            alt="profile"
+            className="object-cover w-full h-full"
+          />
+        ) : (
+          <span className="text-white text-5xl">
+            <AiOutlineUser />
+          </span>
+        )}
+        <label
+          htmlFor="image"
+          className="absolute bottom-0 right-0 -mb-2.5 mr-4 z-10"
+        >
+          <PencilIcon />
+        </label>
+        <input
+          type="file"
+          id="image"
+          accept="image/*"
+          className="hidden"
+          onChange={onChange}
         />
-      ) : (
-        <span className="text-white text-5xl">
-          <AiOutlineUser />
-        </span>
-      )}
-      <label
-        htmlFor="image"
-        className="absolute bottom-0 right-0 -mb-2.5 mr-4 z-10"
-      >
-        <PencilIcon />
-      </label>
-      <input
-        type="file"
-        id="image"
-        accept="image/*"
-        className="hidden"
-        onChange={onChange}
-      />
-    </div>
+      </div>
     </div>
   )
 }
