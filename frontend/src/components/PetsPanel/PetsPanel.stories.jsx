eb39ea1ee1f93f1f4d1c27d22b32fbf996c639fe
Pietro Palombini <pietropalo@icloud.com>
2023-02-27 01:38:51 -0300

    fixup! feat(PetsPanel): create component and story

--- frontend/src/components/PetsPanel/PetsPanel.stories.jsx
+++ frontend/src/components/PetsPanel/PetsPanel.stories.jsx
@@ -4,33 +4,36 @@ import PetsPanel from "."
 import pawfulApi from "../../api/pawfulApi"
 
 const mock = new MockAdapter(pawfulApi)
-mock.onGet("/pets").reply(200, [
-  {
-    id: "id1",
-    name: "Pet 1",
-    photo:
-      "https://media.cnn.com/api/v1/images/stellar/prod/220818142713-dogs-tears-emotions-wellness-stock.jpg?c=original",
-    size: "Large",
-    ageRange: "Puppy",
-    gender: "MALE",
-    profileId: "profileId",
-    type: "DOG",
-    race: "Race 1",
-    about: "More information about pet 1",
-  },
-  {
-    id: "id2",
-    name: "Pet 2",
-    photo:
-      "https://media.istockphoto.com/id/1217828258/photo/grey-stripped-mixed-breed-cat-sitting-isolated-on-white.jpg?s=612x612&w=0&k=20&c=ZdsQKhn9NqMm8KQ-AlpT7D7E0SBv9pNJF-Sbs-j91R0=",
-    size: "Small",
-    ageRange: "Adult",
-    gender: "FEMALE",
-    profileId: "profileId",
-    type: "CAT",
-    race: "Race 2",
-  },
-])
+
+const mockContent = () => {
+  mock.onGet("/pets").reply(200, [
+    {
+      id: "id1",
+      name: "Pet 1",
+      photo:
+        "https://media.cnn.com/api/v1/images/stellar/prod/220818142713-dogs-tears-emotions-wellness-stock.jpg?c=original",
+      size: "Large",
+      ageRange: "Puppy",
+      gender: "MALE",
+      profileId: "profileId",
+      type: "DOG",
+      race: "Race 1",
+      about: "More information about pet 1",
+    },
+    {
+      id: "id2",
+      name: "Pet 2",
+      photo:
+        "https://media.istockphoto.com/id/1217828258/photo/grey-stripped-mixed-breed-cat-sitting-isolated-on-white.jpg?s=612x612&w=0&k=20&c=ZdsQKhn9NqMm8KQ-AlpT7D7E0SBv9pNJF-Sbs-j91R0=",
+      size: "Small",
+      ageRange: "Adult",
+      gender: "FEMALE",
+      profileId: "profileId",
+      type: "CAT",
+      race: "Race 2",
+    },
+  ])
+}
 
 export default {
   title: "Components/PetsPanel",
@@ -45,7 +48,17 @@ export default {
   ],
 }
 
-const Template = (args) => <PetsPanel {...args} />
+export const Empty = () => {
+  mock.onGet("/pets").reply(200, [])
+  return <PetsPanel />
+}
 
-export const Default = Template.bind({})
-Default.args = {}
+export const WithPets = () => {
+  mockContent()
+  return <PetsPanel />
+}
+
+export const Own = () => {
+  mock.onGet("/pets").reply(200, [])
+  return <PetsPanel own />
+}
