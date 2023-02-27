ea8a5245fb37dc71c0e2b29e4cea17170649f500
AXG25 <axiever.guejia@gmail.com>
2023-02-27 16:45:38 -0500

    fix(footer)position

--- frontend/src/components/Register/SuccessfulRegistration.jsx
+++ frontend/src/components/Register/SuccessfulRegistration.jsx
@@ -17,23 +17,26 @@ const SuccessfulRegistration = () => {
   return (
     <div className="global-main-container">
         <NavBar />
-        <h2 className="text-neutral-900 text-title font-title mt-10 mb-4">
+        <h2 className="text-neutral-900 text-center text-title font-title mt-10 mb-4">
           !Te damos la bienvenida a{" "}
           <span className="text-violet-700">Pawful!</span>
         </h2>
         <br />
-        <img src='https://res.cloudinary.com/dw639wmis/image/upload/v1677388098/Registro-exitoso_o1qyq0.png'/>
+        <img src='https://res.cloudinary.com/dw639wmis/image/upload/v1677388098/Registro-exitoso_o1qyq0.png' className="w-60 h-70 m-auto w-full"/>
         <br />        
         
         {/* agregar clase global con padding */}
-        <div id='btnPluslink' className="text-center mt-4 text-violet-700 underline pointer hover:text-violet-900">
+        <div id='btnPluslink' className="text-center mt-4 mb-6">
         <PurpleButton 
           onClick={handleCLick}
           text='Completar perfil'
         />
-          <Link to="/homePage" className="global-link">
+        </div>
+        <div className="text-center pb-10">
+        <Link to="/homePage" className="global-link">
             Hacerlo en otro momento</Link>
         </div>
+        
       </div>
   )
 }
