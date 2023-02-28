f2c9a4f23fe775e20397be58e3d88dca8b5ab6e8
AXG25 <axiever.guejia@gmail.com>
2023-02-27 23:15:35 -0500

    Add(EditProfessionalData)Styles

--- frontend/src/components/IdForm/index.jsx
+++ frontend/src/components/IdForm/index.jsx
@@ -20,9 +20,9 @@ const DniForm = () => {
   }
 
   return (
-    <div className="flex flex-row m-auto max-w-xs">
+    <div className="flex flex-row m-auto max-w-sm w-full mx-4">
       <div className="container flex flex-col justify-center">
-        <label className="py-2" htmlFor="selectDni">
+        <label className="py-2 text-body-bold font-body" htmlFor="selectDni">
           DNI / CUIT
         </label>
         <Select
@@ -32,15 +32,15 @@ const DniForm = () => {
           placeholder={"Opción"}
           value={value}
           onChange={handleChange}
-          className="w-24 text-sm"
+          className="w-24 text-sm bg-slate-50 border-2 h-10 border-neutral-200 focus:outline-none focus:ring focus:ring-violet-700 rounded-lg"
         />
       </div>
       <div className="container flex flex-col justify-center">
-        <label className="py-2" htmlFor="inputNumber">
+        <label className="py-2 text-body-bold font-body" htmlFor="inputNumber">
           Número
         </label>
         <input
-          className="border-2 h-10 border-neutral-200 focus:outline-none focus:ring focus:ring-[#2684FF] rounded-lg pl-2"
+          className="border-2 h-10 w-60 border-neutral-200 focus:outline-none focus:ring focus:ring-[#2684FF] rounded-lg pl-2 bg-slate-50"
           type="text"
           id="inputNumber"
           placeholder="Escriba el número aquí..."
--- frontend/src/components/RegisterProfessional/EditProfessionalData.jsx
+++ frontend/src/components/RegisterProfessional/EditProfessionalData.jsx
@@ -1,41 +1,50 @@
 import React from "react"
 import SimpleText from "../SimpleText/SimpleText"
 import PurpleButton from "../PurpleButton"
-import IdForm from "../IdForm/index" 
+import IdForm from "../IdForm/index"
 import InputForm from "../InputForm/InputForm"
+import NavbarBack from "../../pages/shared/Navbar/NavbarBack"
 
 const EditDataProfessional = () => {
   return (
-    <div className="global-main-container">
-      <div >
-        <SimpleText
-          title={"Datos personales"}
-          paragraph={"Puedes editar o actualizar tu perfil"}
-        />
+    <>
+    <NavbarBack/>
+      <div className="global-main-container">
+        <div >
+          <div className="mt-10 mb-8">
+          <SimpleText
+            title={"Datos personales"}
+            paragraph={"Puedes editar o actualizar tu perfil"}
+          />
+          </div>
 
-        <div className="mb-6">
-          <div>
+          <div className="mb-6">
             <div>
-              <InputForm label={"Nombre completo"} />
+              <div>
+                <InputForm label={"Nombre completo"} />
+              </div>
+            </div>
+
+            <div>
+              <InputForm label={"Dirección"} />
+            </div>
+
+            <div>
+              <InputForm label={"Número de teléfono"} />
+            </div>
+
+            <div className="flex flex-row gap-2">
+              <IdForm />
             </div>
           </div>
 
-          <div>
-            <InputForm label={"Dirección"} />
-          </div>
-
-          <div>
-            <InputForm label={"Número de teléfono"} />
-          </div>
-
-          <div className="flex flex-row gap-2">
-            <IdForm />
+          <div className="pb-16">
+          <PurpleButton text={"Guardar datos"} />
           </div>
         </div>
-
-        <PurpleButton text={"Guardar datos"} />
       </div>
-    </div>
+
+    </>
   )
 }
 
