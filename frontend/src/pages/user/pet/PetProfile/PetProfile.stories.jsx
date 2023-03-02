0136daff77a7cde7ac853ee0f01d440ceb993c7c
AXG25 <axiever.guejia@gmail.com>
2023-03-01 19:00:22 -0500

    add(Petprofile)Styles

--- frontend/src/pages/user/pet/PetProfile/PetProfile.stories.jsx
+++ frontend/src/pages/user/pet/PetProfile/PetProfile.stories.jsx
@@ -9,4 +9,12 @@ export default {
 const Template = (args) => <ProfilePet {...args} />
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  image: "https://res.cloudinary.com/axiever/image/upload/v1677159795/Dog_zagtlo.png",
+  name: "Lucy",
+  raze: "Caniche",
+  owner: "Sabrina",
+  sex: "Hembra",
+  size: "Chico",
+  age: "Adulto"
+}
