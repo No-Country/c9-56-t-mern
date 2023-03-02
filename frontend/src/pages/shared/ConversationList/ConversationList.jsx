0480cf1616771805d453bada4022670610b5d956
AXG25 <axiever.guejia@gmail.com>
2023-03-02 03:28:48 -0500

    add(ConversationList)styles

--- frontend/src/pages/shared/ConversationList/ConversationList.jsx
+++ frontend/src/pages/shared/ConversationList/ConversationList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Contact from '../Contact/Contact'
 
 const ConversationList = () => {
   return (
-    <div>
-      <h2>ConversationList</h2>
+    <div className='mt.10 mb-2 max-w-sm m-auto'>
+      <h2 className='mx-4 text-title font-title'>Chat</h2>
+      <Contact name={'Sabrina Islas'} urlImage={'https://res.cloudinary.com/axiever/image/upload/v1677744895/Profesional-perfil_hlcvtt.png'}/>
+      <Contact name={'Roberta Flores'} urlImage={'https://res.cloudinary.com/axiever/image/upload/v1677744896/User2_jjuqed.png'}/>
     </div>
   )
 }
