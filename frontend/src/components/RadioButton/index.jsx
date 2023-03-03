f70f54bd8e448ca0caeeedafa4317345f5d697ff
AXG25 <axiever.guejia@gmail.com>
2023-03-03 03:38:38 -0500

    restore

--- frontend/src/components/RadioButton/index.jsx
+++ frontend/src/components/RadioButton/index.jsx
@@ -1,12 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 
 const RadioButton = ({ title, desc }) => {
-  return (
-    <button className="bg-neutral-50 w-36 py-3 flex flex-col items-center border-2 border-neutral-200 focus:border-2 focus:border-violet-700 text-neutral-400 focus:text-neutral-900 rounded-lg mx-1 my-1">
-      <h2 className="font-semibold text-base">{title}</h2>
-      <p className="font-normal text-sm">{desc}</p>
-    </button>
-  )
-}
+  const [isChecked, setIsChecked] = useState(false);
 
-export default RadioButton
+  const handleClick = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <>
+      <label 
+        htmlFor={title} 
+        className={`bg-neutral-50 w-40 py-3 flex flex-col items-center border-2 border-neutral-200 
+          ${isChecked ? 'border-violet-700  text-neutral-900 ' : 'text-neutral-400 '} 
+          rounded-lg mx-1 my-1`}
+      >
+        <h2 className="font-body text-body-bold">{title}</h2>
+        <p className="font-caption text-caption">{desc}</p>
+      </label>
+      <input
+        type="radio"
+        id={title}
+        name="name"
+        className="hidden"
+        onClick={handleClick}
+      />
+    </>
+  );
+};
+
+export default RadioButton;
