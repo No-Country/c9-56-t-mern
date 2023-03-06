7b84a4be2c86e4d52d8f1f141e4050d8a9fce99b
Lucero Camila Gonzalez <108992998+lucerocamila@users.noreply.github.com>
2023-03-06 00:32:01 -0300

    run(LoginForm): Prettier format

--- frontend/src/pages/shared/LoginForm/index.jsx
+++ frontend/src/pages/shared/LoginForm/index.jsx
@@ -36,62 +36,61 @@ const LoginForm = () => {
   }, [])
 
   return (
-    <>  
-    <NavbarBack />
-    <div className="flex flex-col max-w-sm mx-auto w-full h-full p-4">
-    
-      <h2 className="text-title font-title mt-10  self-center">
-        Iniciar sesión
-      </h2>
-      <br />
-      <div id="homeMainImage" className="flex items-center justify-center">
-        <img
-          className="w-64 h-40"
-          src="https://images2.imgbox.com/ff/5e/cwCK1YM4_o.png"
-        />
-      </div>
-      <form className="w-full max-w-sm" onSubmit={handleSubmit(onSubmit)}>
-        <div className="#">
-          <InputForm
-            label={"Email o nombre de usuario"}
-            placeholder={"Escribe tu email"}
-            type={"email"}
-            register={register("email", {})}
-          />
-        </div>
-
-        <div className="py-2">
-          <InputForm
-            label={"Contraseña"}
-            placeholder={"Escribe tu contraseña"}
-            type={"password"}
-            register={register("password", {})}
-          />
-        </div>
-
-        <div className="text-end w-full">
-          <a
-            href="#"
-            className="text-link font-link px-4 text-indigo-600 hover:text-indigo-800 underline"
-          >
-            Olvidé mi contraseña
-          </a>
-        </div>
+    <>
+      <NavbarBack />
+      <div className="flex flex-col max-w-sm mx-auto w-full h-full p-4">
+        <h2 className="text-title font-title mt-10  self-center">
+          Iniciar sesión
+        </h2>
         <br />
-        <div className="flex flex-col">
-          <PurpleButton
-            text="Iniciar sesión"
-            type="submit"
+        <div id="homeMainImage" className="flex items-center justify-center">
+          <img
+            className="w-64 h-40"
+            src="https://images2.imgbox.com/ff/5e/cwCK1YM4_o.png"
           />
-          <div className="text-center text-neutral-900 text-caption font caption mt-6">
-            ¿Aún no tienes una cuenta?{" "}
-            <Link to="/register" className="text-indigo-600 hover:text-blue-800 underline text-link font-link ">
-              Registrarte
-            </Link>{" "}
-          </div>
         </div>
-      </form>
-    </div>
+        <form className="w-full max-w-sm" onSubmit={handleSubmit(onSubmit)}>
+          <div className="#">
+            <InputForm
+              label={"Email o nombre de usuario"}
+              placeholder={"Escribe tu email"}
+              type={"email"}
+              register={register("email", {})}
+            />
+          </div>
+
+          <div className="py-2">
+            <InputForm
+              label={"Contraseña"}
+              placeholder={"Escribe tu contraseña"}
+              type={"password"}
+              register={register("password", {})}
+            />
+          </div>
+
+          <div className="text-end w-full">
+            <a
+              href="#"
+              className="text-link font-link px-4 text-indigo-600 hover:text-indigo-800 underline"
+            >
+              Olvidé mi contraseña
+            </a>
+          </div>
+          <br />
+          <div className="flex flex-col">
+            <PurpleButton text="Iniciar sesión" type="submit" />
+            <div className="text-center text-neutral-900 text-caption font caption mt-6">
+              ¿Aún no tienes una cuenta?{" "}
+              <Link
+                to="/register"
+                className="text-indigo-600 hover:text-blue-800 underline text-link font-link "
+              >
+                Registrarte
+              </Link>{" "}
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
