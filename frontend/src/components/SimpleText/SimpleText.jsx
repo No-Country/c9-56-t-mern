85ac73446549d941575e13e66b04196058c5fdae
Lucero Camila Gonzalez <108992998+lucerocamila@users.noreply.github.com>
2023-03-07 03:22:52 -0300

    add(SimpleText): py- dinamic class

--- frontend/src/components/SimpleText/SimpleText.jsx
+++ frontend/src/components/SimpleText/SimpleText.jsx
@@ -1,20 +1,12 @@
 import React from "react"
 
-const SimpleText = ({ title, paragraph }) => {
+const SimpleText = ({ title, paragraph, paddingY }) => {
   return (
     <div className="w-full max-w-sm m-auto px-4 text-center text-neutral-900">
-      <h2 className="text-title font-title mb-2">{title}</h2>
-      <p className="text-subtitle font-subtitle">{paragraph}</p>
+      <h2 className={`text-title font-title lg:text-lg py-${paddingY}`}>{title}</h2>
+      <p className="text-subtitle font-subtitle lg:text-base">{paragraph}</p>
     </div>
   )
 }
 
 export default SimpleText
-
-{
-  //ejemplo de importación
-  /* <SimpleText 
-        title={'Registro'}
-        paragraph={'¡Falta poco! Ya casi tienes tu cuenta en Pawful. '}
-      /> */
-}
