730195de6e4a2d0537e8d479af45a3469ff89525
Lucero Camila Gonzalez <108992998+lucerocamila@users.noreply.github.com>
2023-03-10 00:29:53 -0300

    fix(Card): JioPhone2 view 240x320

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -5,9 +5,9 @@ const Card = ({ imageSide = "left", image, service, paragraph }) => {
   const flexDirection = imageSide === "left" ? "flex-row" : "flex-row-reverse"
 
   return (
-    <div className="flex flex-initial max-w-sm  justify-center">
+    <div className="flex flex-initial max-w-sm justify-center">
       <div
-        className={`flex flex-wrap w-full p-[4%] bg-gray-50 drop-shadow-md rounded-2xl ${flexDirection} items-center text-center text-neutral-900`}
+        className={`flex flex-wrap w-full p-[4%] h-full flex-1 bg-gray-50 drop-shadow-md rounded-2xl ${flexDirection} items-center text-center text-neutral-900`}
       >
         <div className="bg-gray-500 w-24 h-auto flex-initial max-w-sm rounded-full flex-shrink-0">
           <img
@@ -17,7 +17,7 @@ const Card = ({ imageSide = "left", image, service, paragraph }) => {
           />
         </div>
         <div className="flex-1 w-auto h-auto px-1 max-w-sm">
-          <h2 className="text-title font-title">{service}</h2>
+          <b><h2 className="text-title max-[320px]:text-base font-title">{service}</h2></b>
           <p className="text-sans font-subtitle">{paragraph}</p>
         </div>
       </div>
