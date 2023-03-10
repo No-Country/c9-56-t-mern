ba7db76a72ed06f4bee19c73b48302cd641767dd
Lucero Camila Gonzalez <108992998+lucerocamila@users.noreply.github.com>
2023-03-10 01:04:39 -0300

    fix(Card/): Cards that fit their own content

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -7,9 +7,9 @@ const Card = ({ imageSide = "left", image, service, paragraph }) => {
   return (
     <div className="flex flex-initial max-w-sm justify-center">
       <div
-        className={`flex flex-wrap w-full p-[4%] h-full flex-1 bg-gray-50 drop-shadow-md rounded-2xl ${flexDirection} items-center text-center text-neutral-900`}
+        className={`flex flex-wrap flex-1 min-[400px]:w-80 w-full pr-[2%] py-[4%] h-40  bg-gray-50 drop-shadow-md rounded-2xl ${flexDirection} items-center text-center text-neutral-900`}
       >
-        <div className="bg-gray-500 w-24 h-auto flex-initial max-w-sm rounded-full flex-shrink-0">
+        <div className="pl-[3%] w-24 h-auto flex-initial max-w-sm rounded-full flex-shrink-0">
           <img
             src={image}
             alt="service"
@@ -17,7 +17,7 @@ const Card = ({ imageSide = "left", image, service, paragraph }) => {
           />
         </div>
         <div className="flex-1 w-auto h-auto px-1 max-w-sm">
-          <b><h2 className="text-title max-[320px]:text-base font-title">{service}</h2></b>
+          <b><h2 className="text-title max-[300px]:text-base font-title">{service}</h2></b>
           <p className="text-sans font-subtitle">{paragraph}</p>
         </div>
       </div>
